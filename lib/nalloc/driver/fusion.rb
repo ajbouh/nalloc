1d6b1a2f0322af7b9820dabb570cc90455d4345d
mpage <mpage@cs.stanford.edu>
2012-01-19 00:53:50 -0800

    Add cluster destruction
    Add support for destroying clusters via vmrun stop/deleteVM. We release
    the network adapter as soon as the cluster is stopped, however, we
    cannot reclaim disk space until all VMs are no longer running.
    
    Test plan:
    - nalloc-init --destroy
    - Allocated two clusters, destroyed the first, verified its vms
      were no longer running but it was still present on disk. Destroyed
      the second cluster, verified that both were removed from disk.

--- lib/nalloc/driver/fusion.rb
+++ lib/nalloc/driver/fusion.rb
@@ -1,5 +1,6 @@
 require 'erb'
 require 'json'
+require 'set'
 require 'tempfile'
 
 require Nalloc.libpath('nalloc/fusion_support/vmdk_parser')
@@ -57,6 +58,8 @@ class Nalloc::Driver::Fusion < Nalloc::Driver
   #
   # Side effects:
   # - Reserves an adapter from the pool for the lifetime of the cluster.
+  # - Creates a directory to house the vms. This directory lives at
+  #   VM_STORE_PATH/cluster_id
   #
   # @param  [String]  cluster_id  Globally unique cluster id.
   # @param  [Hash]    specs       Node properties. See #start_allocating_node
@@ -68,8 +71,28 @@ class Nalloc::Driver::Fusion < Nalloc::Driver
 
     return {} if specs.empty?
 
+    Nalloc::Caution.attempt("Failed to reap cruft: allocating_nodes") { reap }
+
+    # Create dir to house the vms
+    cluster_dir = File.join(VM_STORE_PATH, cluster_id)
+    unless File.exist?(cluster_dir)
+      FileUtils.mkdir(cluster_dir)
+    end
+
     adapter = @adapter_pool.acquire
     raise "Failed to allocate an adapter" unless adapter
+
+    # If we fail writing out the adapter #destroy_cluster won't be able
+    # to release it, hence the special case error handling here.
+    begin
+      adapter_path = File.join(VM_STORE_PATH, cluster_id, 'adapter.json')
+      File.open(adapter_path, 'w+') do |f|
+        f.write(adapter.to_json)
+      end
+    rescue
+      @adapter_pool.release(adapter)
+      raise
+    end
 
     nodes    = {}
     network  = adapter["subnet"].split(".")[0, 3].join(".")
@@ -78,7 +101,7 @@ class Nalloc::Driver::Fusion < Nalloc::Driver
     host_ctr = 3
     specs.each do |name, spec|
       nodes[name] =
-        start_allocating_node(cluster_id, name, spec,
+        start_allocating_node(cluster_id, name, spec, cluster_dir,
                               :ipaddr     => network + ".#{host_ctr}",
                               :netmask    => adapter["netmask"],
                               :gateway    => gateway,
@@ -89,18 +112,10 @@ class Nalloc::Driver::Fusion < Nalloc::Driver
     end
 
     nodes
-  rescue
-    adapter_pool.release(adapter) if adapter
-
-    raise
   end
 
   # Starts allocating a node that belongs to +cluster_id+ and is specified by
   # +specs+ and +props+
-  #
-  # Side effects:
-  # - Creates a directory to house the vm if it does not already exist. This
-  #   directory lives at VM_STORE_PATH/*cluster_id*
   #
   # @param  [String]  cluster_id  Globally unique cluster id.
   # @param  [String]  name        Node name.
@@ -115,9 +130,10 @@ class Nalloc::Driver::Fusion < Nalloc::Driver
   #                                   name. Corresponding public key will be
   #                                   added to initial user's authorized_keys
   #                                   file.
+  # @param [String]  cluster_dir Where the vm will be housed
   #
   # @return block    Finishes allocating node, blocks until completion.
-  def start_allocating_node(cluster_id, name, specs, props)
+  def start_allocating_node(cluster_id, name, specs, cluster_dir, props)
     vmdk_path = self.class.default_on(specs, :vmdk_path)
     raise "No vmdk_path specified for node #{name}" unless vmdk_path
 
@@ -135,11 +151,6 @@ class Nalloc::Driver::Fusion < Nalloc::Driver
 
     vmx_template_path = self.class.default_on(specs, :vmx_template_path)
     vmx_template_path ||= VMX_TEMPLATE_PATH
-
-    cluster_dir = File.join(VM_STORE_PATH, cluster_id)
-    unless File.exist?(cluster_dir)
-      FileUtils.mkdir(cluster_dir)
-    end
 
     vmx_path = create_vm(vmx_template_path, cluster_dir,
                          :name          => name,
@@ -191,7 +202,65 @@ class Nalloc::Driver::Fusion < Nalloc::Driver
     end
   end
 
+  # Stops running nodes belonging to *cluster_id* and releases the adapter
+  # belonging to the cluster.
+  #
+  # Side effects:
+  #   - If this cluster contains the last running vms then #reap will
+  #     reclaim all disk space used by this cluster, as well as disk
+  #     space used by other cluster that haven't been reaped.
+  #
+  # @param  [String]  cluster_id  Globally unique cluster id
+  #
+  # @return nil
   def destroy_cluster(cluster_id)
+    find_active_nodes().each do |node|
+      next unless node["cluster_id"] == cluster_id
+      vmrun("stop", node["identity"])
+    end
+
+    Nalloc::Caution.attempt("Failed to reap cruft: destroy cluster") { reap }
+
+    nil
+  end
+
+  # Cleans up disk space used by inactive VMs.
+  #
+  # NB: Sadly, this can only make progress if *no* VMs are running as vmrun wil
+  # fail on "deleteVM" with an error claiming that the vm is in use.
+  #
+  # @return nil
+  def reap
+    # Release adapters if possible
+    cluster_glob = File.join(VM_STORE_PATH, '*')
+    all_clusters = Set.new(Dir.glob(cluster_glob).map {|p| File.basename(p) })
+    act_clusters = Set.new(find_active_nodes().map {|n| n["cluster_id"] })
+    inact_clusters = all_clusters - act_clusters
+    inact_clusters.each do |cluster_id|
+      adapter_path = File.join(VM_STORE_PATH, cluster_id, 'adapter.json')
+      if File.exist?(adapter_path)
+        adapter = JSON.parse(File.read(adapter_path))
+        @adapter_pool.release(adapter)
+        FileUtils.rm(adapter_path)
+      end
+    end
+
+    # Can't delete any vms until all vms are down. vmrun will fail claiming
+    # that the vm is in use, regardless of whether or not it is powered on.
+    unless vmrun("list") =~ /running VMs: 0$/
+      return nil
+    end
+
+    to_delete = Dir.glob(File.join(VM_STORE_PATH, '**', '*.vmx'))
+    to_delete.each do |identity|
+      vmrun("deleteVM", identity)
+    end
+
+    # Remove cluster dirs
+    Dir.glob(File.join(VM_STORE_PATH, '*')).each do |cluster_dir|
+      FileUtils.rm_rf(cluster_dir)
+    end
+
     nil
   end
 
@@ -281,5 +350,23 @@ class Nalloc::Driver::Fusion < Nalloc::Driver
     end
 
     nil
+  end
+
+  # Returns all running nodes that have been allocated by nalloc
+  #
+  # @return [Array]  Array of hashes with keys "cluster_id", "identity"
+  def find_active_nodes
+    vms = []
+
+    vmrun("list").split("\n").each do |line|
+      next unless line =~ /^#{VM_STORE_PATH}/
+      parts = line.split(File::SEPARATOR)
+      vms << {
+        "cluster_id" => parts[-3],
+        "identity"   => line,
+      }
+    end
+
+    vms
   end
 end
